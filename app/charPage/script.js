8b3a2b78e5a69ee094f61f47e8bf49f6bd0462c6
Christian Barlera <christian.barlera2@gmail.com>
2022-06-09 19:56:35 -0300

    Finalizando layout charPage

--- app/charPage/script.js
+++ app/charPage/script.js
@@ -12,16 +12,34 @@ async function getCharData(url) {
     const resJSON = await response.json()
 
     document.getElementById('charImg').src = url + imageType
-    document.getElementById('nameTitle').innerText = resJSON['name']
-    document.getElementById('charDescription').innerText = resJSON['description']
-
-    document.getElementById('charName').innerText = resJSON['name']
+    
+    if (resJSON['rarity'] == 4) {
+        for (let index = 0; index < 5; index++) {
+            const raridade = document.getElementById('charRarity')
+            const newIcon = document.createElement('i')
+            if (index == 4) {
+                newIcon.className = 'fi fi-rr-star'
+            } else {
+                newIcon.className = 'fi fi-sr-star'
+            }
+            raridade.appendChild(newIcon)
+        }
+    } else {
+        for (let index = 0; index < 5; index++) {
+            const raridade = document.getElementById('charRarity')
+            const newIcon = document.createElement('i')
+            newIcon.className = 'fi fi-sr-star'
+            raridade.appendChild(newIcon)
+        }
+    }
     document.getElementById('charVision').innerText = resJSON['vision']
     document.getElementById('charWeapon').innerText = resJSON['weapon']
     document.getElementById('charNation').innerText = resJSON['nation']
     document.getElementById('charAffiliation').innerText = resJSON['affiliation']
     document.getElementById('charBirthday').innerText = resJSON['birthday']
-
+    
+    document.querySelector('.descriptionContainer').childNodes[3].childNodes[1].innerText = resJSON['name'] + ":"
+    document.getElementById('charDescription').innerText = resJSON['description']
 }
 
 
